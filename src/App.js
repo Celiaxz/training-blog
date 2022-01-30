1d3c2f0e68befc471d9cab33a6956dd16beb1be1
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2022-01-30 16:07:29 +0100

    first blog creation commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// arrow function, every function returns a HTML(jsx)
+//begin
+import BlogItem from "./BlogItem";
+import "./App.css";
+const myData = [
+  {
+    id: 1,
+    title: "My blog 1",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 2,
+    title: "My blog 2",
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  },
+  {
+    id: 3,
+    title: "My blog 3",
+    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+  {
+    id: 4,
+    title: "My blog 4",
+    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+];
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div>logo</div>
+        <div>
+          <ul className="navs">
+            <li>HOME</li>
+            <li>BLOG</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
       </header>
+
+      <div>
+        <h1>
+          <span className="tittle">The</span> Blog
+        </h1>
+      </div>
+      <BlogItem myData={myData} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/BlogItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BlogItem = (props) => {
+  const { myData } = props;
+  return (
+    <div className="parent-blog-container">
+      {myData.map((item) => {
+        return (
+          <div className="blog-container" key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BlogItem;
