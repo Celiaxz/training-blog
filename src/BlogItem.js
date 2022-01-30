518113fba874ef88a7810494617503d7356b6aa6
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2022-01-30 17:27:02 +0100

    add onHover blue blog border

--- src/BlogItem.js
+++ src/BlogItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./BlogItem.css";
 
 const BlogItem = (props) => {
   const { myData } = props;
   return (
-    <div className="parent-blog-container">
+    <div className="parent-blog-container" id="parentID">
       {myData.map((item) => {
         return (
-          <div className="blog-container" key={item.id}>
+          <div className="blog-container" key={item.id} id="blogID">
             <h2>{item.title}</h2>
             <p>{item.body}</p>
           </div>
