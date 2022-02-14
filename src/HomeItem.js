a64086632f5f2fdebd7bcf88593a1fbad4c7ebc5
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2022-02-14 19:39:04 +0100

    update slide

--- src/HomeItem.js
+++ src/HomeItem.js
@@ -1,6 +1,49 @@
+import React from "react";
+import Slider from "react-slick";
 import "./HomeItem.css";
+const myDate = () => {
+  const currentDate = Date.now();
+  const today = new Date(currentDate);
+  const month = today.toLocaleString("en-us", { month: "long" });
+  const day = today.toLocaleString("en-us", { day: "2-digit" });
+  const year = today.getFullYear();
+  const updateDate = month + " " + day + ", " + year;
+  return updateDate;
+};
 
-const HomeItem = () => {
+const myData = [
+  {
+    id: 1,
+    title: "My blog 1",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 2,
+    title: "My blog 2",
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  },
+  {
+    id: 3,
+    title: "My blog 3",
+    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+  {
+    id: 4,
+    title: "My blog 4",
+    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  },
+];
+
+export default function HomeItem() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "slid",
+  };
+
   return (
     <div>
       <div className="cont">
@@ -15,85 +58,26 @@ const HomeItem = () => {
         </p>
         <p> nullacongue ultrices ultrices vitae orci.</p>
       </div>
-
       {/* Top stories */}
-
       <h1 className="top-story">Top stories</h1>
-
-      <div className="slid">
-        <div className="arrows">
-          <svg
-            width="55"
-            height="95"
-            viewBox="0 0 55 95"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M47.7939 87.9526L7.47632 47.6351L47.7939 7.31755"
-              stroke="black"
-              stroke-opacity="0.42"
-              stroke-width="13.4392"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
-        <div className="rectangle-box">
-          <img src={window.location.origin + "/1.jpg"} alt="computer" />
-
-          <div className="featured-post">
-            <small>May 10, 2022</small>
-            <label>The Importance of Work-Life-Balance</label>
-            <div className="description">
-              Maintaining a healthy work-life balance is importance for your
-              employee's productivity and performance..
+      <Slider {...settings}>
+        {myData.map((slide) => {
+          return (
+            <div className="rectangle-box" key={slide.id}>
+              <img src={window.location.origin + "/1.jpg"} alt="computer" />
+              <div className="featured-post">
+                <small>{myDate()}</small>
+                <label>{slide.title}</label>
+                <div className="description">
+                  Maintaining a healthy work-life balance is importance for your
+                  employee's productivity and performance..
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="arrows">
-          <svg
-            width="55"
-            height="95"
-            viewBox="0 0 55 95"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.47632 87.9526L47.7939 47.6351L7.47632 7.31755"
-              stroke="black"
-              stroke-opacity="0.42"
-              stroke-width="13.4392"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
-      </div>
-
-      <div className="ellipsis">
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="10" cy="10" r="10" fill="black" />
-        </svg>
-
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="10" cy="10" r="10" fill="#C4C4C4" />
-        </svg>
-      </div>
+          );
+        })}
+      </Slider>
+      ;
     </div>
   );
-};
-
-export default HomeItem;
+}
